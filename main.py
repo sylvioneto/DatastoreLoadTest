81c1425695725fbb8f0a58a609aebb7abb5244c0
sylvio.pedroza <sylvio.pedroza@gmail.com>
2022-10-21 11:43:12 -0300

    test with concurrency

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import uuid
 from datetime import datetime
 from faker import Faker
 import os
+import concurrent.futures
+
 
 # Datastore details
 PROJECT_ID=os.getenv("PROJECT_ID")
@@ -20,34 +22,39 @@ def load_test():
     print("This script will upsert {} records to Datastore".format(NUMBER_OF_ENTITIES))
 
     # create fake data
+    print("Creating batches with fake data...")
     batches = []
     while len(batches) < (NUMBER_OF_ENTITIES/COMMIT_SIZE):
         batches.append(create_fake_entity(COMMIT_SIZE))
     
-    # Instantiates a client
-    client = datastore.Client(project=PROJECT_ID, namespace=NAMESPACE)
-
     start_time = datetime.now()
-    print(start_time)
+    print("Start time {}".format(start_time))
 
-    batch_count = 0
-    for b in batches:
-        batch_count+=1
-        tasks = []
-        for t in b:
-            task = datastore.Entity(client.key(KIND, t['order_id']))
-            task.update(t)
-            tasks.append(task)
-        client.put_multi(tasks)
-        print("Batch {} done".format(batch_count))
+    poolBatches(batches)
     
     end_time = datetime.now()
-    print(end_time)
+    print("End time {}".format(end_time))
     
     delta = end_time - start_time
     print("Time taken to upsert {0} records was {1} seconds".format(NUMBER_OF_ENTITIES, delta.total_seconds()))
     print("Average of {0} records per second".format(NUMBER_OF_ENTITIES/delta.total_seconds()))
     print(f"Done")
+
+
+def poolBatches(batches):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(load_batch_to_datastore, batches)
+
+
+def load_batch_to_datastore(batch):
+    client = datastore.Client(project=PROJECT_ID, namespace=NAMESPACE)
+    upsert_list = []
+    for o in batch:
+        entity = datastore.Entity(client.key(KIND, o['order_id']))
+        entity.update(o)
+        upsert_list.append(entity)
+    client.put_multi(upsert_list)
+    print("Upsert done!")
 
 
 def create_fake_entity(num_of_entities):
